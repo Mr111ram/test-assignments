e8f30736aa68941fe7874f279c046fd2785ba2df
Kirill Katelyanets <katelyanets@gmail.com>
2023-02-15 22:28:13 +0800

    add undo task

---
+++ undo/makeId.js
@@ -0,0 +1,15 @@
+function makeId(length = 8) {
+  let result = ''
+  let counter = 0
+  const characters =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  const charactersLength = characters.length
+
+  while (counter < length) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength))
+    counter += 1
+  }
+  return result
+}
+
+window.makeId = makeId
